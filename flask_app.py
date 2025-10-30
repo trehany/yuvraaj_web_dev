a0716dc569aa3e765631eb93c5b33493199ad910
trehany <159546033+trehany@users.noreply.github.com>
2025-10-30 16:41:36 +0000

    more additions

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title':'Database engineer',
+    'location': 'Delhi',
+    'salary': 'Rs 200000000' 
+  },
+  {
+       
+    'id':2,
+    'title': 'engineer',
+    'location': 'Delhi', 
+  }
+  
+]
+    
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0',debug = True)
